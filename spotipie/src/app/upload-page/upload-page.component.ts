628d370011d96c05e68be44fac489f9d3e2a2825
iammaxence <maxencebrunet91@gmail.com>
2021-04-09 20:06:52 +0200

    Fix bug and wrap user interaction error

--- spotipie/src/app/upload-page/upload-page.component.ts
+++ spotipie/src/app/upload-page/upload-page.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'upload-page-component',
@@ -12,6 +10,7 @@ import { catchError, retry } from 'rxjs/operators';
 export class UploadPageComponent implements OnInit {
 
   isvalid: Boolean = false;
+  isvalidSize: Boolean = true;
 
   constructor(private router:Router, private http: HttpClient) { }
 
@@ -26,15 +25,29 @@ export class UploadPageComponent implements OnInit {
     if(this.isvalid)
       this.router.navigate(['home']);
     else
-      console.log("Invalid zip ! ")
+      console.log("Invalid zip ! "+this.isvalid)
   }
 
+
+  /**
+   * 
+   * @param event 
+   * @returns "Success" if the file have been upload successfuly
+   */
   public uploadFileToServer(event:any) {
     let fileList: FileList = event.target.files;
-    console.log(fileList)
+    let file: File = fileList[0];
 
-    if (fileList.length > 0) {
-      let file: File = fileList[0];
+    // If the size of the file is >20mb, it's not a valid file
+    if(!this.fileSizeValidation(file)){
+      this.isvalidSize=false;
+      return;
+    }
+    else {
+      this.isvalidSize=true;
+    }
+
+    if (fileList.length > 0 ) {
       let formData: FormData = new FormData();
       formData.append('uploadFile', file, file.name);
       formData.append('fileType', 'zip');
@@ -49,6 +62,23 @@ export class UploadPageComponent implements OnInit {
         error => console.log(error)
         )
     }
+    
   } 
+  
+  /**
+   * 
+   * @param file 
+   * @returns True if the size is valid, else False
+   */
+  private fileSizeValidation(file: File){
+    
+    const fileSizeInKB = Math.round(file.size / 1024)/1000;
+    console.log(fileSizeInKB);
+    if (fileSizeInKB >= 20) {
+      return false;
+    }
+    
+    return true;
+  }
 
 }
